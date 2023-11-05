6fe6d7fa043250b5c5f93f84eb624ebc26add02b
justiniantheemperor <justinrellis52@gmail.com>
2023-11-05 14:24:38 -0700

    about section changes

--- src/pages/Home/sections/About.js
+++ src/pages/Home/sections/About.js
@@ -14,24 +14,20 @@ Coded by www.creative-tim.com
 */
 
 // @mui material components
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import { Box, Container, Grid, Typography } from "@mui/material";
 
 // Material Kit 2 React components
-import MKBox from "components/MKBox";
-import MKInput from "components/MKInput";
 import MKButton from "components/MKButton";
-import MKTypography from "components/MKTypography";
 
 // Images
 import bgImage from "assets/images/headshots/main.jpg";
 
-function Contact() {
+function About() {
   return (
-    <MKBox component="section" py={{ xs: 0, lg: 6 }}>
+    <Box py={{ xs: 0, lg: 6 }}>
       <Container>
         <Grid container item>
-          <MKBox
+          <Box
             width="100%"
             bgColor="white"
             borderRadius="xl"
@@ -45,42 +41,51 @@ function Contact() {
                 xs={12}
                 lg={5}
                 position="relative"
-                px={0}
-                height="25rem"
+                width="100%"
                 sx={{
-                  backgroundImage: `url(${bgImage})`,
-                  backgroundSize: "cover",
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignContent: "center",
+                  justifyContent:"center",
                 }}
               >
-                <MKBox
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  width="100%"
-                  height="100%"
-                ></MKBox>
+                <Box
+                  component="img"
+                  alt="Devoree Headshot"
+                  src={bgImage}
+                  height="30rem"
+                />
               </Grid>
-              <Grid item xs={12} lg={7}>
-                <MKBox px={3} py={{ xs: 2, sm: 6 }}>
-                  <MKTypography variant="h2" mb={1}>
+              <Grid
+                item
+                xs={12}
+                lg={7}
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignContent: "center",
+                }}
+              >
+                <Box px={3} py={{ xs: 2, sm: 6 }}>
+                  <Typography variant="h2" mb={1}>
                     About Me
-                  </MKTypography>
-                  <MKTypography variant="body1" color="text" mb={2}>
+                  </Typography>
+                  <Typography variant="body1" color="text" mb={2}>
                     Devoree is from Orange County, California. She completed her
                     training in April 2024, receiving a BFA in Acting from
                     Brigham Young University.
-                  </MKTypography>
+                  </Typography>
                   <MKButton variant="gradient" color="info">
                     Send Message
                   </MKButton>
-                </MKBox>
+                </Box>
               </Grid>
             </Grid>
-          </MKBox>
+          </Box>
         </Grid>
       </Container>
-    </MKBox>
+    </Box>
   );
 }
 
-export default Contact;
+export default About;
