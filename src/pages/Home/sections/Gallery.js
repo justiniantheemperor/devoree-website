6b635dbf1d863978d609dde6fe87477eadb708bf
justiniantheemperor <justinrellis52@gmail.com>
2023-11-10 20:16:27 -0700

    gallery change

--- src/pages/Home/sections/Gallery.js
+++ src/pages/Home/sections/Gallery.js
@@ -2,33 +2,35 @@ import React from "react";
 import Carousel from "react-material-ui-carousel";
 import "pages/Home/styles/gallery.css";
 
-import { Card, CardMedia, Grid, Container } from "@mui/material";
+import { useMediaQuery, Card, CardMedia, Grid, Container } from "@mui/material";
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 
 // Headshots
-// import main1 from "assets/images/headshots/Devoree.1a.jpg";
-// import main2 from "assets/images/headshots/Devoree.1b.jpg";
-// import main3 from "assets/images/headshots/Devoree.1c.jpg";
+import main1 from "assets/images/headshots/Devoree.1a.jpg";
+import main2 from "assets/images/headshots/Devoree.1b.jpg";
+import main3 from "assets/images/headshots/Devoree.1c.jpg";
 
-// import dark1 from "assets/images/headshots/Devoree.2a.jpg";
-// import dark2 from "assets/images/headshots/Devoree.2b.jpg";
-// import dark3 from "assets/images/headshots/Devoree.2c.jpg";
-// import dark4 from "assets/images/headshots/Devoree.2d.jpg";
-// import dark5 from "assets/images/headshots/Devoree.2e.jpg";
+import dark1 from "assets/images/headshots/Devoree.2a.jpg";
+import dark2 from "assets/images/headshots/Devoree.2b.jpg";
+import dark3 from "assets/images/headshots/Devoree.2c.jpg";
+import dark4 from "assets/images/headshots/Devoree.2d.jpg";
+import dark5 from "assets/images/headshots/Devoree.2e.jpg";
 // import dark6 from "assets/images/headshots/Devoree.2f.jpg";
-// import dark7 from "assets/images/headshots/Devoree.2g.jpg";
+import dark7 from "assets/images/headshots/Devoree.2g.jpg";
 
-// import happy1 from "assets/images/headshots/Devoree.3a.jpg";
+import happy1 from "assets/images/headshots/Devoree.3a.jpg";
 // import happy2 from "assets/images/headshots/Devoree.3b.jpg";
-// import happy3 from "assets/images/headshots/Devoree.3c.jpg";
-// import happy4 from "assets/images/headshots/Devoree.3d.jpg";
+import happy3 from "assets/images/headshots/Devoree.3c.jpg";
+import happy4 from "assets/images/headshots/Devoree.3d.jpg";
 
 function Banner(props) {
+  const isSmallScreen = useMediaQuery("(max-width:770px)");
+
   const totalItems = props.length ? props.length : 3;
-  const mediaLength = totalItems;
+  const mediaLength = isSmallScreen ? 1 : totalItems;
 
   let items = [];
 
@@ -36,7 +38,7 @@ function Banner(props) {
     const item = props.item.Items[i];
 
     const media = (
-      <Grid item xs={12 / totalItems} key={i}>
+      <Grid item xs={12 / mediaLength} key={i}>
         <CardMedia
           className="Media"
           image={item.Image}
@@ -61,39 +63,52 @@ const items = [
   {
     Items: [
       {
-        Image: "https://source.unsplash.com/featured/?macbook",
+        Image: main1,
       },
       {
-        Image: "https://source.unsplash.com/featured/?iphone",
+        Image: dark1,
       },
       {
-        Image: "https://source.unsplash.com/featured/?vacuum,cleaner",
-      },
-    ],
-  },
-  {
-    Items: [
-      {
-        Image: "https://source.unsplash.com/featured/?washingmachine",
-      },
-      {
-        Image: "https://source.unsplash.com/featured/?macbook",
-      },
-      {
-        Image: "https://source.unsplash.com/featured/?vacuum,cleaner",
+        Image: happy3,
       },
     ],
   },
   {
     Items: [
       {
-        Image: "https://source.unsplash.com/featured/?lamp",
+        Image: dark3,
       },
       {
-        Image: "https://source.unsplash.com/featured/?vase",
+        Image: main3,
       },
       {
-        Image: "https://source.unsplash.com/featured/?macbook",
+        Image: dark7,
+      },
+    ],
+  },
+  {
+    Items: [
+      {
+        Image: happy1,
+      },
+      {
+        Image: dark4,
+      },
+      {
+        Image: main2,
+      },
+    ],
+  },
+  {
+    Items: [
+      {
+        Image: dark5,
+      },
+      {
+        Image: happy4,
+      },
+      {
+        Image: dark2,
       },
     ],
   },
@@ -102,7 +117,7 @@ const items = [
 class BannerExample extends React.Component {
   state = {
     autoPlay: true,
-    animation: "fade",
+    animation: "slide",
     indicators: true,
     timeout: 500,
     navButtonsAlwaysVisible: false,
@@ -154,42 +169,40 @@ class BannerExample extends React.Component {
 
   render() {
     return (
-      // <MKBox
-      //   component="section"
-      //   variant="gradient"
-      //   bgColor="dark"
-      //   position="relative"
-      //   py={6}
-      //   px={{ xs: 2, lg: 0 }}
-      //   mx={-2}
-      // >
-        <Container>
-          <Grid item xs={12} md={8} sx={{ mb: 6, textAlign: "center" }}>
-            <MKTypography variant="h3" color="white">
-              Gallery
-            </MKTypography>
-            <MKTypography variant="body2" color="white" opacity={0.8}>
-              See more of my headshots and photos from previous productions.
-            </MKTypography>
-          </Grid>
-          <Card>
-            <Carousel
-              className="Example"
-              autoPlay={this.state.autoPlay}
-              animation={this.state.animation}
-              indicators={this.state.indicators}
-              timeout={this.state.timeout}
-              cycleNavigation={this.state.cycleNavigation}
-              navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
-              navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
-            >
-              {items.map((item, index) => {
-                return <Banner item={item} key={index} />;
-              })}
-            </Carousel>
-          </Card>
-        </Container>
-      // </MKBox>
+      <Container>
+        <Grid item xs={12} md={8} sx={{ mb: 6, textAlign: "center" }}>
+          <MKTypography variant="h3" color="white">
+            Gallery
+          </MKTypography>
+          <MKTypography variant="body2" color="white" opacity={0.8}>
+            See more of my headshots and photos from previous productions.
+          </MKTypography>
+        </Grid>
+        <Card
+          sx={{ background: "linear-gradient(195deg, #858585, #454545)"}}
+          >
+          <Carousel
+            className="Example"
+            autoPlay={this.state.autoPlay}
+            animation={this.state.animation}
+            indicators={this.state.indicators}
+            timeout={this.state.timeout}
+            cycleNavigation={this.state.cycleNavigation}
+            navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
+            navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
+            duration={400}
+            activeIndicatorIconButtonProps={{
+              style: {
+                color: "#E7827F", 
+              },
+            }}
+          >
+            {items.map((item, index) => {
+              return <Banner item={item} key={index} />;
+            })}
+          </Carousel>
+        </Card>
+      </Container>
     );
   }
 }
